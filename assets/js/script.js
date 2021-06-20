9b671b9d103e62e955a28e9b81fc5f2c89d28e0d
brandonmongo <brandonmongo@hotmail.com>
2021-06-20 15:41:55 +0000

    added pop up sign up form

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,30 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+
+// sign up form
+var signup = document.getElementById("pride-signup")
+
+var signupBtn = document.getElementById("pride-btn")
+
+var closeSignup = document.getElementById("close-btn")
+
+
+// When the user clicks the button, open the sign up form
+signupBtn.onclick = function() {
+  signup.style.display = "block";
+}
+
+// When the user clicks on the close button the sign up form, close it
+closeSignup.onclick = function() {
+    signup.style.display = "none";
+}
+
+
+// When the user clicks anywhere outside of the sign up form, close it (no working now)
+window.onclick = function(event) {
+  if (event.target == this.signup) {
+    this.signup.style.display = "none";
+  }
+}
